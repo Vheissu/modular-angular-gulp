2c7cd775bd65c775033995a6cfc96356f80a6937
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-01-16 10:21:42 +1000

    Added in AngularJS, default index file and home controller module/file

---
+++ src/modules/home/controllers/homectrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var HomeCtrl = function($scope) {
+    $scope.testVariable = 'This is a test variable value being populated from the home module controller';
+};
+
+module.exports = HomeCtrl;
